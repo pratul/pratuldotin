d21a9ee964c0ca6299f59473455e5350ba981b23
Pratul Kalia <pratul@pratul.in>
2010-12-29 04:22:11 +0530

    Fixed comments.

--- comment.tpl.php
+++ comment.tpl.php
@@ -1,20 +1,31 @@
 <?php
 // Copyright 2010 (c) Pratul Kalia
+// print "<pre>";
+// print_r($comment);
+// print "</pre>";
+if ($comment->homepage) {
+  $url = $comment->homepage;
+}
+if ($comment->uid == 1) {
+  $u = " self";
+}
 ?>
-<div class="comment<?php print ($comment->new) ? ' comment-new' : ''; print ' '. $status; print ' '. $zebra; ?>">
+<div class="comment<?php print ($comment->new) ? ' comment-new' : ''; print ' '. $status; print ' '. $zebra;?><?php print $u ?>">
 
-  <div class="clear-block">
-  <?php if ($submitted): ?>
-    <span class="submitted"><?php print $submitted; ?></span>
-  <?php endif; ?>
+    <div class="clear-block">
+    <?php if ($submitted): ?>
+      <div class="submitted">On <?php print $date ?>, 
+        <?php if ($comment->homepage): ?>
+          <a href="<?php print $comment->homepage ?>">
+        <?php endif; ?>
+          <?php print $comment->name ?>
+        <?php if ($comment->homepage): print "</a>"; endif;?> said…</div>
+    <?php endif; ?>
 
   <?php if ($comment->new) : ?>
     <span class="new"><?php print drupal_ucfirst($new) ?></span>
   <?php endif; ?>
 
-  <?php print $picture ?>
-
-    <h3><?php print $title ?></h3>
 
     <div class="content">
       <?php print $content ?>
